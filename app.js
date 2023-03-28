e5b85e0a739887c97ab8e41f1b003251ffc67ac3
parasswarup <parasswarup@gmail.com>
2023-03-28 11:05:57 -0400

    login done

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors'
+import UserController from "./users/users-controller.js";
 const app = express();
 app.use(cors())
 app.use(express.json());
 app.get('/hello', (req, res) => {res.send('Life is good!')})
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+
+UserController(app);
 app.listen(process.env.PORT || 4000);
---
+++ users/users-controller.js
@@ -0,0 +1,97 @@
+import users from "./users.js";
+
+const UserController = (app) => {
+    let currentUser = null;
+    const findAllUsers = (req, res) => {
+        if (currentUser && currentUser.role === "ADMIN") {
+            res.json(users);
+        } else {
+            res.sendStatus(403);
+        }
+    };
+    const findUserById = (req, res) => {
+        const userId = req.params.userId;
+        const user = users.find((user) => user._id === userId);
+        if (user) {
+            res.json(user);
+        } else {
+            res.sendStatus(404);
+        }
+    };
+
+    const createUser = (req, res) => {
+        const user = { ...req.body, _id: new Date().getTime() + "" };
+        users.push(user);
+        res.json(user);
+    };
+    const updateUser = (req, res) => {
+        const userId = req.params.userId;
+        const newUser = req.body;
+        const index = users.findIndex((user) => user._id === userId);
+        if (index === -1) {
+            res.sendStatus(404);
+            return;
+        }
+        users[index] = newUser;
+        res.sendStatus(200);
+    };
+    const deleteUser = (req, res) => {
+        const userId = req.params.userId;
+        const index = users.findIndex((user) => user._id === userId);
+        if (index === -1) {
+            res.sendStatus(404);
+            return;
+        }
+        users.splice(index, 1);
+        res.sendStatus(200);
+    };
+
+    const register = (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        const user = users.find((user) => user.username === username);
+        if (user) {
+            res.sendStatus(409);
+            return;
+        }
+        const newUser = { username, password, _id: new Date().getTime() + "" };
+        currentUser = newUser;
+        users.push(newUser);
+        res.json(newUser);
+    };
+
+
+    app.post("/api/users/register", register);
+    app.post("/api/users/login", (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        const user = users.find(
+            (user) => user.username === username && user.password === password
+        );
+        if (user) {
+            currentUser = user;
+            res.json(user);
+        } else {
+            res.sendStatus(404);
+        }
+    });
+    app.post("/api/users/profile", (req, res) => {
+        if (!currentUser) {
+            res.sendStatus(404);
+            return;
+        }
+        res.json(currentUser);
+    });
+    app.post("/api/users/logout", (req, res) => {
+        currentUser = null;
+        res.sendStatus(200);
+    });
+
+    app.get("/api/users", findAllUsers);
+    app.get("/api/users/:userId", findUserById);
+    app.post("/api/users", createUser);
+    app.put("/api/users/:userId", updateUser);
+    app.delete("/api/users/:userId", deleteUser);
+};
+
+export default UserController;
