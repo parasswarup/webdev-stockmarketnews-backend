ad6d522a4b2905eef99dca92e2acfc88379b70a8
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2023-04-23 20:27:09 -0400

    news structure change

--- mongooseSchema/news-comments.js
+++ mongooseSchema/news-comments.js
@@ -2,8 +2,7 @@ import mongoose,{Schema} from "mongoose"
 
 const NewsCommentsSchema = new mongoose.Schema({
     user:{type:Schema.Types.ObjectId,ref:"users"},
-    news:{type:String},
+    news:{type:Object,ref:"news"},
     comment:{type:String},
     date:{type:Date}},{collection: 'news-comments'})
-
 export default NewsCommentsSchema
